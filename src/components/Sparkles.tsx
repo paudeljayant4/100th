520039dc3c323b2cca44706d4a2add2735a77b96
paudeljayant4 <paudeljayant4@gmail.com>
2026-04-26 18:13:49 +0545

    Add Sparkles component to render twinkling sparkle effects

---
+++ src/components/Sparkles.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sparkles.css';
+
+const Sparkles = () => {
+  return (
+    <div className="sparkle-container">
+      <div className="sparkle"></div>
+      <div className="sparkle"></div>
+      <div className="sparkle"></div>
+      <div className="sparkle"></div>
+      <div className="sparkle"></div>
+    </div>
+  );
+};
+
+export default Sparkles;
